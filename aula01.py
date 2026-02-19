3d8deb7bdf93b16c90b89af6338e3ac86bf3a36d
Bianca Pereira <bianca.pinheiro01089337@sesisenaipr.org.br>
2026-02-19 10:05:36 -0300

    Primeira aula de programação

---
+++ aula01.py
@@ -0,0 +1,9 @@
+idade = 26 # variável inteira
+peso = 80.5 # variável float
+nome = 'Bianca Pereira Pinheiro' # variável string 
+sexo_feminino = True # variável bool
+
+print(nome)
+print(idade)
+print(peso)
+print(sexo_feminino)
