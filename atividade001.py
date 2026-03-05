7d7ab01e77e1f6da3cdb5e7fca2138f29d388a6d
bianca <bianca.pinheiro01089337@sesisenaipr.gov.br>
2026-03-05 10:12:05 -0300

    aula

---
+++ atividade001.py
@@ -0,0 +1,12 @@
+comprimento = input("cumprimento da peça está entre 10cm a 12cm?: ")
+
+if comprimento == "sim":
+    lagura = input("Largura da peça esta entre 5cm a 6cm?: ")
+    if lagura == "sim" : 
+        print("peça aprovada!")
+        else:
+            print("reprovado! problema na largura")
+else:
+    print("reprovado! peça invalida.")
+    
+
