d1e2ae9293d5d69957422fe94d9c0111c37ffa88
Bianca <bianca.pinheiro01098337@sesisenaipr.org.br>
2026-03-31 12:02:19 -0300

    aula

---
+++ lista001.py
@@ -0,0 +1,5 @@
+nota01 = float(input("digite sua nota 1: "))
+nota02 = float(input("digite sua nota 2: "))
+nota03 = float(input("digite sua nota 3: "))
+soma = (nota01 + nota02 + nota03) / 3
+print("sua soma é", soma )
