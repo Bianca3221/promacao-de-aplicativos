13a881976f6b8517d47849e423dae0a598ed889d
Bianca <bianca.pinheiro01089337@sesisenaipr.org.br>
2026-05-05 12:00:37 -0300

    Aula 05/05

---
+++ projeto_crud.py
@@ -0,0 +1,56 @@
+estoque = []
+acao = 0
+
+# adiciona o item dentro da lista 
+def adicionar_lista (nome,estoque):
+    estoque.append(nome)
+
+# vai listar o produto desejado
+def listar_produtos(estoque):
+    for nome in estoque:
+        indice = estoque.index(nome)
+        print(f"posição: {indice}, Nome: {nome}")
+        
+
+def atualizar_produto(indice,novo_nome,estoque):
+    estoque[indice] = novo_nome
+
+def remover_produto(indice):
+    remover = estoque.pop(indice)
+    print(f"item {remover} removido!")
+
+# mostra as opções q existem dentro do menu para executar as funções 
+def exibir_menu(acao):
+    while acao != 5:
+        print("PROGRAMA INICIANDO")
+        print("1- Adicionar produto")
+        print("2- Listar produto")
+        print("3- Atualizar produto")
+        print("4- Remover produto da lista ")
+        print("5- Sair")
+
+        acao = input("Digite a ação desejada: ")
+
+        if acao == 1 :
+            produto = int(input("Digite o nome do produto: "))
+            adicionar_lista(produto,estoque)
+
+        elif acao == 2 :
+            listar_produtos(estoque)
+        
+        elif acao == 3 :
+            atualizar = input("Digite o nome do produto que deseja atualizar: ")
+            indice = estoque.index(atualizar)
+            nome_novo = input("Digite o nome do novo produto: ")
+            atualizar_produto(indice,nome_novo,estoque)
+
+        elif acao == 4 :
+            remover = input("Digite o nome do produto que deseja remover: ")
+            indice = estoque.index(remover)
+            remover_produto(indice)
+       
+        elif acao == 5 :
+            print("Programa encerrado!")
+            return
+
+exibir_menu(acao)
