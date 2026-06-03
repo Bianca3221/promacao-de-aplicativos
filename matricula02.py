fe532b51b52435c521aee2c528d8c8483ae74db5
Bianca <bianca.pinheiro01089337@sesisenaipr.org.br>
2026-06-03 12:04:26 -0300

    Aula 02/06

--- matricula02.py
+++ matricula02.py
@@ -51,6 +51,72 @@ def listar():
     for aluno in alunos: 
         print(f"Nome: {aluno['nome']} | CPF: {aluno['cpf']} | Turma: {aluno['turma']} | Tel: {aluno['telefone']}")
 
+def atualizar ():
+    print("----ATUALIZAR-----")
+    if not os.path.exists(MATRICULA):
+         print("Nenhum aluno cadastrado no sistema.") 
+         return
+    
+    with open(MATRICULA, 'r', encoding='utf-8') as f: 
+        alunos  = json.load(f)
+
+    cpf_busca = int(input("Digite o CPF do aluno que deseja editar: "))
+    
+    for aluno in alunos:
+        if aluno['cpf'] == cpf_busca: 
+            print(f"Editando dados de: {aluno['nome']}")
+            aluno['nome'] = input(f"Novo Nome ({aluno['nome']}): ") or aluno['nome']
+            aluno['telefone'] = input(f"Novo Telefone ({aluno['telefone']}): ") or aluno['telefone']
+            aluno['turma'] = input(f"Nova Turma ({aluno['turma']}): ") or aluno['turma']
+            aluno['idade'] = int(input(f"Nova Idade ({aluno['idade']}): ") or aluno['idade'])
+            aluno['cpf'] = input(f"Novo CPF ({aluno['cpf']}): ") or aluno['cpf']
+
+            with open(MATRICULA, 'w', encoding='utf-8') as f:    
+                json.dump(alunos, f, indent=4, ensure_ascii=False)   
+            print("Dados atualizados com sucesso!")
+            return
+        
+    print("Aluno não encontrado.")
+
+
+def excluir():
+    print("\n--- Excluir Aluno ---")
+    if not os.path.exists(MATRICULA):
+        print("Nenhum aluno cadastrado no sistema.")
+        return
+    with open (MATRICULA, 'r', encoding='utf-8') as f:
+        alunos = json.load(f) 
+
+    id_busca = int(input("Digite o ID do aluno que deseja remover: "))
+
+    nova_lista = [a for a in alunos if a['id'] != id_busca]
+    
+    if len(nova_lista) < len(alunos):
+        with open(MATRICULA, 'w', encoding='utf-8') as f:
+             json.dump([], f)
+
+def menu():
+    while True:
+        print("\n=== SISTEMA ESCOLAR ===") 
+        print("1. Cadastrar Aluno")
+        print("2. Listar Alunos") 
+        print("3. Atualizar Aluno")  
+        print("4. Excluir Aluno") 
+        print("5. Sair") 
+
+        opcao = input("Escolha uma opção: ")
+
+        if opcao == '1': cadastrar()  
+        elif opcao == '2': listar()
+        elif opcao == '3': atualizar()  
+        elif opcao == '4': excluir()
+        elif opcao == '5': break 
+        else: print("Opção inválida!")
+ 
+menu()
+    
+
+
         
         
     
